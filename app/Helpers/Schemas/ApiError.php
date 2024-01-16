d69f50baa5e08706c75691aefbdf48f7643cc2f9
Felipe Silva <felipe.allware@gmail.com>
2024-01-16 20:31:24 -0300

    feat: Criando o schema da DTO de erros da documentação

---
+++ app/Helpers/Schemas/ApiError.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Helpers\Schemas;
+
+/**
+ * @OA\Schema(
+ *      schema="ApiError",
+ *      @OA\Property(property="message", type="string", example="Ocorreu um erro.")
+ * )
+ */
+class ApiError {
+
+}
