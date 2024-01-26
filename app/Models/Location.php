36acf8311c5283f98c24738d9a68d4da38006bd2
felipe.silva <felipe.silva@pwi.com.br>
2024-01-26 18:29:01 -0300

    docs: Documentação da model Location

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -5,6 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @OA\Schema(
+ *      schema="Location",
+ *      required={"title"},
+ *      @OA\Property(property="id", type="integer", example=1),
+ *      @OA\Property(property="customer_id", type="integer", example=1),
+ *      @OA\Property(property="film_id", type="integer", example=1),
+ *      @OA\Property(property="date", type="string", format="date", example="2024-01-19")
+ * )
+ */
 class Location extends Model
 {
     use HasFactory;
