db1cafe52cc1c78aa5a81055a33b14725f200105
Felipe Silva <felipe.allware@gmail.com>
2024-02-21 21:38:34 -0300

    docs: Documentando restando do controller de Customer

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -15,28 +15,88 @@ class CustomerController extends MasterController
         $this->request = $request;
     }
 
-    
+    /**
+     * @OA\Get(
+     *      path="/api/customer",
+     *      tags={"Customer"},
+     *      summary="List all customers",
+     *      @OA\Parameter(
+     *          name="id",
+     *          in="query",
+     *          description="Customer ID",
+     *          @OA\Schema(type="integer")
+     *      ),
+     *      @OA\Parameter(
+     *          name="name",
+     *          in="query",
+     *          description="Customer name",
+     *          @OA\Schema(type="string")
+     *      ),
+     *      @OA\Parameter(
+     *          name="cpf_cnpj",
+     *          in="query",
+     *          description="Customer CPF/CNPJ",
+     *          @OA\Schema(type="string")
+     *      ),
+     *      @OA\Parameter(
+     *          name="image",
+     *          in="query",
+     *          description="Customer image name",
+     *          @OA\Schema(type="string")
+     *      ),
+     *      @OA\Response(
+     *          response="200", 
+     *          description="Customer list",
+     *          @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/Customer")
+     *         )
+     *      ),
+     *      @OA\Response(
+     *          response="401", 
+     *          description="Unauthorized",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiError")
+     *      ),
+     *     security={{"bearerAuth":{}}}
+     * )
+     */
     public function index(Request $request) {
-        $query = $this->model::query();
-        $filters = $request->all();
-
-        $othersFillableFields = ['id', 'created_at', 'updated_at'];
-
-        foreach ($filters as $field => $value) {
-            if (in_array($field, $this->model->getFillable()) || in_array($field, $othersFillableFields)) {
-                if (gettype($value) == 'string') {
-                    $query->where($field, 'like', '%'.trim($value).'%');
-                } else {
-                    $query->where($field, $value);
-                }
-            }
-        }
-
-        $data = $query->paginate(20);
-
-        return response()->json($data, 200);
+        return parent::index($request);
     }
 
+    /**
+     * @OA\Get(
+     *      path="/api/customer/{id}/telephone",
+     *      tags={"Customer"},
+     *      summary="List an customer with telephones",
+     *      @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="Customer ID",
+     *         @OA\Schema(type="integer")
+     *      ),
+     *      @OA\Response(
+     *          response="200", 
+     *          description="Customer data",
+     *          @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/CustomerWithTelephoneDTO")
+     *         )
+     *      ),
+     *      @OA\Response(
+     *          response="401", 
+     *          description="Unauthorized",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiError")
+     *      ),
+     *      @OA\Response(
+     *          response="404", 
+     *          description="Record not found",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiError")
+     *      ),
+     *     security={{"bearerAuth":{}}}
+     * )
+     */
     public function telephone($id) {
         $data = $this->model::with('telephone')->find($id);
 
